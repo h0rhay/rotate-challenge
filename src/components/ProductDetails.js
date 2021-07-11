2eba7bea26876b445a7b6235e378c217d2415331
George Clark <clarkgeorge76@gmail.com>
2021-07-11 14:06:54 +0100

    styling product details

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -3,10 +3,34 @@ import React from 'react'
 const ProductDetails = () => {
   return (
     <section>
-      <h2>Product Title</h2>
-      <p>Some info about the product</p>
-      <hr />
+      <div className="text-group text-group-primary">
+        <h2>Product Title</h2>
+        <p>Some info about the product</p>
+      </div>
+      <div className="text-group">
+        <p className='bold'>Product Title</p>
+        <p>Some info about the product</p>
+      </div>
+      <div className="text-group">
+        <p className='bold'>Product Title</p>
+        <p>Some info about the product</p>
+      </div>
+      <div className="text-group">
+        <p className='bold'>Product Title</p>
+        <p>Some info about the product</p>
+      </div>
+      
       <p>Some more info about it</p>
+      <form>
+        <label>
+          <input type="radio" name="radio" checked/>
+          <span>100 ml</span>
+        </label>
+        <label>
+          <input type="radio" name="radio" checked/>
+          <span>200 ml</span>
+        </label>
+      </form>
       <button>BUY!</button>
     </section>
   )
